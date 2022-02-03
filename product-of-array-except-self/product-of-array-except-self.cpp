4641313dbbd9597b72301389634216314870ab4a
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-02-03 12:14:23 +0530

    Time: 49 ms, Memory: 25 MB - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -11,18 +11,13 @@ public:
         
         int right_product = 1;
         
-        for(int i=nums.size()-1;i>=0;i--){
-            
-            if(i==nums.size()-1){
-                result[i]=result[i-1];
-            }else if(i==0){
-                result[i]=right_product;
-            }else{
-                result[i]=result[i-1]*right_product;
-                
-            }
+        for(int i=nums.size()-1;i>0;i--){
+        
+            result[i]=result[i-1]*right_product;
             right_product=right_product*nums[i];
         }
+        
+        result[0]=right_product;
         
         return result;
         }
