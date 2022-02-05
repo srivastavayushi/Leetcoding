fd7b0a2358b2a7f74d93e982e58e23fe85612525
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-02-05 12:23:54 +0530

    Time: 4 ms (99.89%), Space: 13.2 MB (53.81%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,24 +4,28 @@ public:
         
         int rows = matrix.size();
         int columns = matrix[0].size();
-        
-        vector<int>dummy_row(rows,-1);
-        vector<int>dummy_column(columns,-1);
+        bool col0 = true;
         
         for(int i=0;i<matrix.size();i++){ 
-            for(int j=0;j<matrix[i].size();j++){
+            if(matrix[i][0]==0){
+                col0 = false;
+            }
+            for(int j=1;j<matrix[i].size();j++){
                 if(matrix[i][j]==0){
-                    dummy_row[i]=0;
-                    dummy_column[j]=0;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
         
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(dummy_row[i]==0 || dummy_column[j]==0){
+        for(int i=rows-1;i>=0;i--){
+            for(int j=columns-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
+            }
+            if(col0==false){
+                matrix[i][0]=0;
             }
         }
         
