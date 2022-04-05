cc4dd0da471172c82ad58febeb2fa1699fee1a2d
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-04-05 20:48:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.99%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n>0){
+            if(n%2==1){
+               res++; 
+            }
+            n/=2;
+        } 
+        return res;
+    }
+};
