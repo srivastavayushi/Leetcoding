7db19dae36933a4bf568574c3e0ee9c6c229e404
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-06-09 20:39:14 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -23,7 +23,7 @@ public:
         for(int i=1;i<dp.size();i++){
             for(int j=1;j<dp[0].size();j++){
                 if(arr[i-1]<=j){
-                    dp[i][j] = max(dp[i-1][j-arr[i-1]],dp[i-1][j]);
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
