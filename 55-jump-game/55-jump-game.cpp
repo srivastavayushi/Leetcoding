44e5dd77d3eb8a1b8a6bb802f2d2a6cf100a9f2b
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-07-09 09:47:00 +0530

    Time: 1536 ms (5.15%), Space: 51.1 MB (6.79%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n = nums.size()-1;
+    vector<int> dp;
+    bool jump(vector<int>&nums,int idx){
+        if(idx == nums.size()-1) return true;
+        if(idx>=nums.size()) return false;
         
-        int reachableIdx = 0;
+        if(dp[idx] != -1) return dp[idx];
         
-        for(int i=0;i<=n;i++){
-            if(reachableIdx<i){
-                return false;
-            }
-            reachableIdx = max(reachableIdx,nums[i]+i);
+        for(int i=1;i<=nums[idx];i++){
+            if(jump(nums,idx+i)) return dp[idx] = true;
         }
-        
-        return true;
+        return dp[idx] = false;
+    }
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==1) return true;
+        dp.assign(nums.size(),-1);
+        bool res = jump(nums,0);
+        // for(auto it : mpp) cout<<it.first<<' '<<it.second<<endl;
+        // cout<<"----"<<endl;
+        return res;
     }
 };
