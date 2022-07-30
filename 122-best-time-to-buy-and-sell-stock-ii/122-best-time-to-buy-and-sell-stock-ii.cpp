e0f6dcc6c247040c33b442e92811fc87e8bb68ba
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-07-30 23:02:13 +0530

    Time: 14 ms (40.01%), Space: 13.1 MB (57.47%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,18 +3,18 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        
-        // every cell in matrix suggests : agar is index ko buy/not buy kia to kitta price hoga
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        
-        dp[n][1] = dp[n][0] = 0;
+    
+        int buy = 0;
+        int notbuy = 0;
         
         for(int i=n-1;i>=0;i--){
-            // buy - 1 and !buy - 0
-            dp[i][1] = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-            dp[i][0] = max(prices[i] + dp[i+1][1], dp[i+1][0]);
+            int temp_buy = max(-prices[i] + notbuy, buy);
+            int temp_notbuy = max(prices[i] + buy, notbuy);
+            buy = temp_buy;
+            notbuy = temp_notbuy;
+            
         }
         
-        return dp[0][1];
+        return buy;
     }
 };
