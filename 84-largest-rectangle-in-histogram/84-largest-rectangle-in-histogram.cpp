cfb050d9f1f94f3d7725307707e0a65647994495
Ayushi Srivastava <Ayushimanoj1201@gmail.com>
2022-08-13 12:23:40 +0530

    Time: 208 ms (86.55%), Space: 81.4 MB (52.23%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    //  Get Nearest smaller ele to left and right + 1
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        stack<int>st;
+        
+        // next smaller element to left
+        for(int i=0;i<n;i++){
+            
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            
+            if(st.empty()) left[i] = 0;
+            else left[i] = st.top() + 1;
+            
+            st.push(i);
+        }
+        
+        while(!st.empty()) st.pop();
+        
+        // next smaller element to right
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            
+            if(st.empty()) right[i] = n-1;
+            else right[i] = st.top() - 1;
+            
+            st.push(i);
+        }
+        
+//         for(auto it:right) cout<<it<<' ';
+//         cout<<endl;
+        
+//         for(auto it:left) cout<<it<<' ';
+//         cout<<endl;
+        int res = 0;
+        for(int i=0;i<n;i++){
+            res = max(res,((right[i]-left[i])+1)*heights[i]);
+        }
+        
+        return res;
+        
+    }
+};
